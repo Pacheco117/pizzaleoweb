75743913e4eb2779ac139f4130f45f581967363e
Pacheco117 <68613915+Pacheco117@users.noreply.github.com>
2024-04-22 11:05:28 -0600

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root","","pizzaleo");
+$conexion = mysqli_connect("server.mysql.database.azure.com", "ikolzumiuv","3=DL3Z%v+c,9*;N","pizzaleo-database");
 
 
 /*
